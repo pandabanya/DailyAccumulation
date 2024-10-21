b5002ea33396d05760783a3de2e29bbc48355b80
pandabanya <782120844@qq.com>
2024-10-21 16:59:46 +0800

    feat -m "重构"

---
+++ vue2/demo-vue2/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="teleport-title">传送门</div>
+    <div class="teleport-list">
+      <el-row :gutter="20">
+        <el-col :span="6"><div class="grid-content ">
+          <router-link to="demoBanner">swiper banner</router-link>
+        </div></el-col>
+        <el-col :span="6"><div class="grid-content ">
+
+        </div></el-col>
+        <el-col :span="6"><div class="grid-content ">222</div></el-col>
+        <el-col :span="6"><div class="grid-content ">333</div></el-col>
+      </el-row>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+};
+</script>
+
+<style>
+#app {
+  overflow: hidden;
+}
+</style>
